21486cd28fbac4de7e9c739aaf9ea41d770e428c
Nibesh Khadka <nibeshkhadka@gmail.com>
2018-01-23 08:11:23 +0200

    Number guessing game with UI

---
+++ NumberWizard-UI/NumberWizard-UI/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("Requesting for next level to load.");
+		Application.LoadLevel(name);
+
+		}
+
+	public void QuitRequest(){
+		Debug.Log("Requesting for quitting game.");
+		Application.Quit();
+
+	}
+}
---
+++ NumberWizard-UI/NumberWizard-UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	int max; 
+    int min;
+    int guess;
+    int maxGuess=15;
+    public Text text;
+
+	void Start () {
+		StartGame();
+	}
+
+
+	public void GuessHigher(){
+			min=guess;
+           NextGuess();
+	}
+	public void GuessLower(){
+			max=guess;
+           NextGuess();
+	}
+
+	void StartGame(){
+		 max = 10000;
+	   	 min = 1;
+	     NextGuess();
+	
+	}
+	void NextGuess(){
+        guess=Random.Range(min,max+1);
+        text.text=guess.ToString();
+        maxGuess=maxGuess-1;
+        if(maxGuess<=0){
+        	Application.LoadLevel("Win");
+        }
+    }
+}
+ 
